382ba2fa53516da4046471d2471cd3db57a68f0e
Argus <argushakizimana@gmail.com>
2024-05-29 01:22:53 +0200

    new way

--- index.js
+++ index.js
@@ -19,27 +19,58 @@
 // let port = process.env.PORT || 3000;
 // app.listen(port, () => console.log(`App listening on ${port}`));
 
-const path = require("path");
-const webpack = require("webpack");
-const WebpackDevServer = require("webpack-dev-server");
-const webpackConfig = require("./webpack.config");
+// const path = require("path");
+// const webpack = require("webpack");
+// const WebpackDevServer = require("webpack-dev-server");
+// const webpackConfig = require("./webpack.config");
 
-const webpackDevServerOptions = {
-  publicPath: webpackConfig.output.publicPath,
-  contentBase: path.join(process.cwd(), "dist"),
-  historyApiFallback: true,
-  hot: true,
-  host: "0.0.0.0",
-  open: true,
-  compress: true,
-  quiet: true // This ensures that FriendlyErrorsWebpackPlugin can show clean output
+// const webpackDevServerOptions = {
+//   publicPath: webpackConfig.output.publicPath,
+//   contentBase: path.join(process.cwd(), "dist"),
+//   historyApiFallback: true,
+//   hot: true,
+//   host: "0.0.0.0",
+//   open: true,
+//   compress: true,
+//   quiet: true // This ensures that FriendlyErrorsWebpackPlugin can show clean output
+// };
+
+// WebpackDevServer.addDevServerEntrypoints(webpackConfig, webpackDevServerOptions);
+// const compiler = webpack(webpackConfig);
+// const server = new WebpackDevServer(compiler, webpackDevServerOptions);
+
+// const port = process.env.PORT || 3000;
+// server.listen(port, "0.0.0.0", () => {
+//   console.log(`App listening on http://localhost:${port}`);
+// });
+
+const Webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const webpackConfig = require('./webpack.config.js');
+
+// Prepare the compiler
+const compiler = Webpack(webpackConfig);
+
+// Extract and extend the development server options from your webpack config
+const devServerOptions = { ...webpackConfig.devServer, open: true };
+
+// Create a new instance of WebpackDevServer
+const server = new WebpackDevServer(devServerOptions, compiler);
+
+// Function to start the server
+const runServer = async () => {
+  console.log('Starting server...');
+  await server.start();
 };
 
-WebpackDevServer.addDevServerEntrypoints(webpackConfig, webpackDevServerOptions);
-const compiler = webpack(webpackConfig);
-const server = new WebpackDevServer(compiler, webpackDevServerOptions);
+// Function to stop the server with a callback
+const stopServer = async () => {
+  console.log('Stopping server...');
+  await server.stop();
+};
 
-const port = process.env.PORT || 3000;
-server.listen(port, "0.0.0.0", () => {
-  console.log(`App listening on http://localhost:${port}`);
-});
+// Start the server
+runServer();
+
+// Optionally, set a timeout to stop the server, e.g., after 5000 milliseconds
+// setTimeout(stopServer, 5000);
